73733f93b9fe779c6504a7e64b576be65f202125
sujalshrestha9906 <suzalstha1@gmail.com>
2023-03-15 17:29:00 +0545

    three

--- lib/services/crud_services.dart
+++ lib/services/crud_services.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
@@ -10,28 +12,29 @@ import '../model/user.dart';
 
 class CrudService {
   static Dio dio = Dio();
-  static Future<Either<String, bool>> ucreateProduct(
+  static Future<Either<String, bool>> createProduct(
       {required String product_name,
       required String product_detail,
       required int price,
-      required String imageUrl,
-      required String public_id,
+      required String token,
       required XFile image}) async {
     try {
       final cloudinary =
-          CloudinaryPublic('CLOUD_NAME', 'UPLOAD_PRESET', cache: false);
+          CloudinaryPublic('dau6uv53k', 'musc7x0t', cache: false);
 
       CloudinaryResponse res = await cloudinary.uploadFile(
         CloudinaryFile.fromFile(image.path,
             resourceType: CloudinaryResourceType.Image),
       );
-      final response = await dio.post(Api.addProduct, data: {
-        'product_name': product_name,
-        'product_detail': product_detail,
-        'price': price,
-        'imageUrl': imageUrl,
-        'public_id': public_id
-      });
+      final response = await dio.post(Api.addProduct,
+          data: {
+            'product_name': product_name,
+            'product_detail': product_detail,
+            'price': price,
+            'imageUrl': res.secureUrl,
+            'public_id': res.publicId
+          },
+          options: Options(headers: {HttpHeaders.authorizationHeader: token}));
 
       final user = User.fromJson(response.data);
       final box = Hive.box<User>('user');
